15e0707230227add1e1258d2f27aed9aad970ed1
Maksym Fedorov <m.fedorov@anchorfree.com>
2021-12-28 09:33:29 +0200

    Implement ReverseString

---
+++ src/ReverseString.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Problems
+{
+    internal sealed class ReverseString
+    {
+        public static void Run(char[] s)
+        {
+            for (int i = 0; i < s.Length / 2; i++)
+            {
+                var temp = s[i];
+                s[i] = s[s.Length - i - 1];
+                s[s.Length - i - 1] = temp;
+            }
+        }
+    }
+}
---
+++ tests/ReverseStringTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestClass]
+    public sealed class ReverseStringTests
+    {
+        [TestMethod]
+        [DataRow(new[] { 'h', 'e', 'l', 'l', 'o' }, new[] { 'o', 'l', 'l', 'e', 'h' })]
+        [DataRow(new[] { 'H', 'a', 'n', 'n', 'a', 'h' }, new[] { 'h', 'a', 'n', 'n', 'a', 'H' })]
+        public void Run_ReturnsReversedString(char[] s, char[] expected)
+        {
+            // Act
+            ReverseString.Run(s);
+            var actual = s;
+
+            // Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
