03f3d6de8a43caef79509c075f21ac4037f16f78
maksym-fedorov-zero <mfedorov@zerosystems.com>
2024-12-01 16:41:43 +0200

    Initial commit

---
+++ src/BestTimeToBuyAndSellStock.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Problems
+{
+    public sealed class BestTimeToBuyAndSellStock
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            return 0;
+        }
+    }
+}
---
+++ tests/BestTimeToBuyAndSellStockTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Tests
+{
+    [TestClass]
+    public sealed class BestTimeToBuyAndSellStockTests
+    {
+        [TestMethod]
+        [DynamicData(nameof(GetTestData), DynamicDataSourceType.Method)]
+        public void MaxProfit_ShouldReturnExpected(int[] prices, int expected)
+        {
+            // Arrange
+            // Act
+            var actual = BestTimeToBuyAndSellStock.MaxProfit(prices);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        private static IEnumerable<object[]> GetTestData()
+        {
+            yield return new object[] { new int[] { 7, 1, 5, 3, 6, 4 }, 5 };
+            yield return new object[] { new int[] { 7, 6, 4, 3, 1 }, 0 };
+            yield return new object[] { new int[] { 2, 4, 1 }, 2 };
+        }
+    }
+}
