d4d987bc5bd87ef3d4e974663c1f19a2a57d5746
Maksym Fedorov <maksym.fedorov@aura.com>
2024-12-02 23:31:31 +0200

    Implement solution

--- src/BestTimeToBuyAndSellStock.cs
+++ src/BestTimeToBuyAndSellStock.cs
@@ -1,10 +1,36 @@
-﻿namespace LeetCode.Problems
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Problems
 {
     public sealed class BestTimeToBuyAndSellStock
     {
         public static int MaxProfit(int[] prices)
         {
-            return 0;
+            int buy = prices[0];
+            int sell = prices[prices.Length - 1];
+            int maxProfit = sell > buy ? sell - buy : 0;
+
+            for (int i = 1; i < prices.Length; i++)
+            {
+                if (prices[i] < buy)
+                {
+                    buy = prices[i];
+                    sell = 0;
+                }
+                else if (prices[i] > sell)
+                {
+                    sell = prices[i];
+                    int profit = sell - buy;
+
+                    if (maxProfit < profit)
+                    {
+                        maxProfit = profit;
+                    }
+                }
+            }
+
+            return maxProfit;
         }
     }
 }
--- tests/BestTimeToBuyAndSellStockTests.cs
+++ tests/BestTimeToBuyAndSellStockTests.cs
@@ -20,9 +20,11 @@ namespace LeetCode.Problems.Tests
 
         private static IEnumerable<object[]> GetTestData()
         {
-            yield return new object[] { new int[] { 7, 1, 5, 3, 6, 4 }, 5 };
-            yield return new object[] { new int[] { 7, 6, 4, 3, 1 }, 0 };
+            yield return new object[] { new int[] { 1, 2 }, 1 };
             yield return new object[] { new int[] { 2, 4, 1 }, 2 };
+            yield return new object[] { new int[] { 2, 1, 4 }, 3 };
+            yield return new object[] { new int[] { 7, 6, 4, 3, 1 }, 0 };
+            yield return new object[] { new int[] { 7, 1, 5, 3, 6, 4 }, 5 };
         }
     }
 }
